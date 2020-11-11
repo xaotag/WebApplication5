e371c0fa6082b9da102da03a279b63ac233b31d2
xaotag <3036905349@qq.com>
2020-11-11 13:39:00 +0800

    add login files

---
+++ Command/SqlTools.cs
@@ -0,0 +1,52 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebApplication5.Command
+{
+    public class SqlTools
+    {
+        /// <summary>
+        /// 单例模式
+        /// </summary>
+        private static SqlConnection GloblasqlConnection = null;
+
+        private static void conntiConnection()
+        {
+            string conn = ConfigurationManager.ConnectionStrings["xaotag"].ConnectionString;
+            if (GloblasqlConnection == null)
+            {
+                GloblasqlConnection = new SqlConnection(conn);
+            }
+        }
+
+        /// <summary>
+        /// 执行sql语句 增删改
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static int commandBySql(string sql)
+        {
+            conntiConnection();
+            SqlConnection sqlConnection = GloblasqlConnection;
+            SqlCommand scd = new SqlCommand(sql, sqlConnection);
+            sqlConnection.Open();
+            int flag = scd.ExecuteNonQuery();
+            sqlConnection.Close();
+            return flag;
+        }
+        /// <summary>
+        /// 查询语句
+        /// </summary>
+        /// <param name="sql"></param>
+        /// <returns></returns>
+        public static DataSet SqlDataSet(string sql)
+        {
+            conntiConnection();
+            SqlDataAdapter data = new SqlDataAdapter(sql, GloblasqlConnection);
+            DataSet ds = new DataSet();
+            data.Fill(ds);
+            return ds;
+        }
+    }
+}
---
+++ login.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication5.Command;
+
+namespace WebApplication5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            long mobile = Convert.ToInt64(TextBox1.Text.Trim());
+            string pwd = TextBox2.Text.Trim();
+            string Sql = "SELECT StaffID,Mobile,pwd FROM Info_Staff WHERE Mobile =  N'" + mobile+ "' and Pwd = N'" + pwd + "' and IsDelete =0;";
+            DataSet ds = SqlTools.SqlDataSet(Sql);
+            if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Mobile"].ToString().ToString()))
+            {
+                
+            }
+
+        }
+    }
+}
