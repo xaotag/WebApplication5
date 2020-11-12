c2f19dba49388cc46476310fd49c0fd51bdc129e
xaotag <3036905349@qq.com>
2020-11-12 21:41:32 +0800

    add login page

---
+++ UserModel.cs
@@ -0,0 +1,28 @@
+﻿namespace WebApplication5
+{
+    public class UserModel
+    {
+        private string Guid;
+        private string mobile;
+        private string pwd;
+
+
+        public string Guid1
+        {
+            get => Guid;
+            set => Guid = value;
+        }
+
+        public string Mobile
+        {
+            get => mobile;
+            set => mobile = value;
+        }
+
+        public string Pwd
+        {
+            get => pwd;
+            set => pwd = value;
+        }
+    }
+}
--- login.aspx.cs
+++ login.aspx.cs
@@ -18,14 +18,50 @@ namespace WebApplication5
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            
             long mobile = Convert.ToInt64(TextBox1.Text.Trim());
             string pwd = TextBox2.Text.Trim();
             string Sql = "SELECT StaffID,Mobile,pwd FROM Info_Staff WHERE Mobile =  N'" + mobile+ "' and Pwd = N'" + pwd + "' and IsDelete =0;";
             DataSet ds = SqlTools.SqlDataSet(Sql);
-            if (!string.IsNullOrEmpty(ds.Tables[0].Rows[0]["Mobile"].ToString().ToString()))
+            string UserMobile = ds.Tables[0].Rows[0]["Mobile"].ToString();
+            if (!string.IsNullOrEmpty(UserMobile))
             {
-                
+                if (UserMobile=="18438570098")
+                {
+                    Response.Redirect("UserListInfo.aspx");
+                }
+                else
+                {
+                    Response.Redirect("OrderList.aspx");
+                }
+                Response.Write("<script>alert('账号或密码错误')<script>");
             }
+            
+            /*
+             后面有用
+            List<UserModel> userModel = new List<UserModel>();
+            string Sql = "SELECT StaffID,Mobile,pwd FROM Info_Staff WHERE IsDelete =0;";
+            DataSet ds = SqlTools.SqlDataSet(Sql);
+            foreach (DataRow DataRow in ds.Tables[0].Rows)
+            {
+                UserModel m
+                    odel = new UserModel();
+                foreach (DataColumn column in ds.Tables[0].Columns)
+               ，8  {
+                    switch (column.ColumnName)
+                    {
+                        case "StaffID":
+                            model.Guid1 = DataRow["Guid"].ToString(); break;
+                        case "Mobile":
+                            model.Mobile = DataRow["Mobile"].ToString(); break;
+                        case "Pwd":
+                            model.Pwd = DataRow["Pwd"].ToString(); break;
+                    }
+                    userModel.Add(model);
+                }
+            }
+            */
+
 
         }
     }
